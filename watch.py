ce9e93f99f416c8ab48e00ef0c182c7a29372e50
hellflame <hellflamedly@gmail.com>
2017-09-10 08:45:52 +0800

    interval shorten

--- watch.py
+++ watch.py
@@ -9,7 +9,7 @@ try:
         print "{} | m: {:>2}".format((" " * 3).join('{:b}'.format(t.tm_min).rjust(6)), t.tm_min)
         print "{} | s: {:>2}".format((" " * 3).join('{:b}'.format(t.tm_sec).rjust(6)), t.tm_sec)
         sys.stdout.write("\033[F" * 3)
-        time.sleep(1)
+        time.sleep(.2)
 except KeyboardInterrupt:
     sys.stdout.write("\n" * 3)
 
