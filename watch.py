1e21ac68ab04ea12b43cc255e3050aab3703f314
hellflame <hellflamedly@gmail.com>
2017-09-10 08:33:31 +0800

    align right

--- watch.py
+++ watch.py
@@ -5,9 +5,9 @@ import time
 try:
     while True:
         t = time.localtime()
-        print "{} | h: {}".format((" " * 4).join('{:b}'.format(t.tm_hour).rjust(5)), t.tm_hour)
-        print "{} | m: {}".format((" " * 3).join('{:b}'.format(t.tm_min).rjust(6)), t.tm_min)
-        print "{} | s: {}".format((" " * 3).join('{:b}'.format(t.tm_sec).rjust(6)), t.tm_sec)
+        print "{} | h: {:>2}".format((" " * 4).join('{:b}'.format(t.tm_hour).rjust(5)), t.tm_hour)
+        print "{} | m: {:>2}".format((" " * 3).join('{:b}'.format(t.tm_min).rjust(6)), t.tm_min)
+        print "{} | s: {:>2}".format((" " * 3).join('{:b}'.format(t.tm_sec).rjust(6)), t.tm_sec)
         sys.stdout.write("\033[F" * 3)
         time.sleep(1)
 except KeyboardInterrupt:
