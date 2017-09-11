6f13e08063fae8e492660134421fdc0ca9a97331
hellflame <hellflamedly@gmail.com>
2017-09-11 08:24:25 +0800

    colorful

--- watch.py
+++ watch.py
@@ -2,14 +2,27 @@ from __future__ import print_function
 import sys
 import time
 
+__version__ = '1.0.0'
+__author__ = 'hellflame'
 
+
+if sys.version_info.major == 2:
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+
+COLORS = ['\033[01;34m{}\033[00m', '\033[01;34m{}\033[00m',
+          '\033[01;35m{}\033[00m', '\033[01;35m{}\033[00m',
+          '\033[01;31m{}\033[00m',
+          '\033[01;36m{}\033[00m', '\033[01;36m{}\033[00m',
+          '\033[01;32m{}\033[00m', '\033[01;32m{}\033[00m',
+          '\033[01;33m{}\033[00m', '\033[01;33m{}\033[00m']
 THEMES = {
     'raw': None,
     'smallBox': (u'\u25A1', u'\u25A0'),
     'bigBox': (u'\u25A2', u'\u25A9'),
-    'boxSimple': (' ', u'\u25A3'),
-    'circleSimple': (' ', u'\u25C9'),
-    'rhombusSimple': (' ', u'\u25C8')
+    'boxSimple': ('-', u'\u25A3'),
+    'circleSimple': ('-', u'\u25C9'),
+    'rhombusSimple': ('-', u'\u25C8')
 }
 
 
@@ -24,15 +37,29 @@ def to_matrix(n):
     return [[tmp[index][i] for index in range(len(tmp))] for i in range(4)]
 
 
-def theme_print(theme, text):
-    for line in text:
-        if THEMES.get(theme):
-            print(" ".join([THEMES[theme][int(x)] for x in line]))
+def theme_print(theme, text, color=False):
+    if color:
+        if len(text[0]) == 11:
+            for line in text:
+                if THEMES.get(theme):
+                    print(" ".join([COLORS[i].format(THEMES[theme][int(x)]) for i, x in enumerate(line)]))
+                else:
+                    print(" ".join([COLORS[i] for i, x in enumerate(line)]))
         else:
-            print(" ".join(line))
+            for line in text:
+                if THEMES.get(theme):
+                    print(" ".join([COLORS[i + 5].format(THEMES[theme][int(x)]) for i, x in enumerate(line)]))
+                else:
+                    print(" ".join([COLORS[i + 5].format(x) for i, x in enumerate(line)]))
+    else:
+        for line in text:
+            if THEMES.get(theme):
+                print(" ".join([THEMES[theme][int(x)] for x in line]))
+            else:
+                print(" ".join(line))
 
 
-def glimpse(theme='boxSimple', full=False, hint=False):
+def glimpse(theme='boxSimple', full=False, hint=False, color=False):
     t = time.localtime()
     if theme == 'basic':
         # Not So Pretty
@@ -46,15 +73,14 @@ def glimpse(theme='boxSimple', full=False, hint=False):
                 print("{}".format((" " * 7).join("{:b}".format(t.tm_mon).rjust(4))))
                 print("{}".format((" " * 5).join("{:b}".format(t.tm_mday).rjust(5))))
                 print("{}".format((" " * 7).join("{:b}".format(t.tm_wday + 1).rjust(4))))
+        if hint:
+            print("{} |  Hour : {:>2}".format((" " * 5).join('{:b}'.format(t.tm_hour).rjust(5)), t.tm_hour))
+            print("  {}   |   Min : {:>2}".format((" " * 3).join('{:b}'.format(t.tm_min).rjust(6)), t.tm_min))
+            print("  {}   |   Sec : {:>2}".format((" " * 3).join('{:b}'.format(t.tm_sec).rjust(6)), t.tm_sec))
         else:
-            if hint:
-                print("{} |  Hour : {:>2}".format((" " * 5).join('{:b}'.format(t.tm_hour).rjust(5)), t.tm_hour))
-                print("  {}   |   Min : {:>2}".format((" " * 3).join('{:b}'.format(t.tm_min).rjust(6)), t.tm_min))
-                print("  {}   |   Sec : {:>2}".format((" " * 3).join('{:b}'.format(t.tm_sec).rjust(6)), t.tm_sec))
-            else:
-                print("{}".format((" " * 5).join('{:b}'.format(t.tm_hour).rjust(5))))
-                print("  {}   ".format((" " * 3).join('{:b}'.format(t.tm_min).rjust(6))))
-                print("  {}   ".format((" " * 3).join('{:b}'.format(t.tm_sec).rjust(6))))
+            print("{}".format((" " * 5).join('{:b}'.format(t.tm_hour).rjust(5))))
+            print("  {}   ".format((" " * 3).join('{:b}'.format(t.tm_min).rjust(6))))
+            print("  {}   ".format((" " * 3).join('{:b}'.format(t.tm_sec).rjust(6))))
     elif theme in THEMES:
         # Kinda
         if full:
@@ -66,35 +92,41 @@ def glimpse(theme='boxSimple', full=False, hint=False):
                                                                                   sec=t.tm_sec)
             result = to_matrix(raw)
             if hint:
-                theme_print(theme, result)
+                theme_print(theme, result, color)
                 print("_" * 21)
                 print('M M D D W h h m m s s')
                 print(' '.join(raw))
             else:
-                theme_print(theme, result)
+                theme_print(theme, result, color)
         else:
             raw = '{hour:0>2}{min:0>2}{sec:0>2}'.format(hour=t.tm_hour, min=t.tm_min, sec=t.tm_sec)
             result = to_matrix(raw)
             if hint:
-                theme_print(theme, result)
+                theme_print(theme, result, color)
                 print("_" * 11)
                 print('h h m m s s')
                 print(' '.join(raw))
             else:
-                theme_print(theme, result)
+                theme_print(theme, result, color)
 
 
-def loop_watch(theme='basic', full=True, hint=True):
+def loop_watch(theme='basic', full=True, hint=True, color=False):
     """
     start watch click, control-C to stop
-    :param theme: String
-    :param full: Boolean
-    :param hint: Boolean
+    :param theme: print style choice
+    :param full: set True to print full length date time string
+    :param hint: set True to print human-readable date time or else, programmer-readable date time
+    :param color: set True to print colorful columns, more easy to read
+
+    :type theme: String
+    :type full: Boolean
+    :type hint: Boolean
+    :type color: Boolean
     :return: None
     """
     try:
         while True:
-            glimpse(theme, full, hint)
+            glimpse(theme, full, hint, color)
             if theme == 'basic':
                 if full:
                     sys.stdout.write("\033[F" * 6)
@@ -120,4 +152,4 @@ def loop_watch(theme='basic', full=True, hint=True):
 
 
 if __name__ == '__main__':
-    loop_watch('circleSimple', hint=True, full=True)
+    loop_watch('boxSimple', hint=False, full=False, color=False)
