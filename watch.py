72e0b61b4e4ad1ec1b0112ef499764def36a3201
hellflame <hellflamedly@gmail.com>
2017-09-12 15:07:54 +0800

    add TODO

--- watch.py
+++ watch.py
@@ -60,6 +60,7 @@ def transpose(n):
     :return: list of 4 lists
     """
     tmp = ['{:0>4}'.format("{:b}".format(int(s))) for s in n]
+    # TODO:: convert list to tuple
     return [[tmp[index][i] for index in range(len(tmp))] for i in range(4)]
 
 
@@ -235,6 +236,7 @@ def terminal():
 
 
 if __name__ == '__main__':
+    # TODO:: Test switch
     if True:
         # default to unit test
         import unittest
